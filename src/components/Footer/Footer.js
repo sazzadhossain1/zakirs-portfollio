fc0c1607288d3bd3a00da895de395e730b12811d
sazzad tomal <sazzadtomal1@gmail.com>
2025-11-24 03:46:03 +0600

    footer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,40 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>This is footer</h1>
+    <div className="footer_parent_div">
+      <div className="footer_grid_div">
+        <div>
+          <h3>Lorem ipsum dolor sit amet.</h3>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+        </div>
+        <div>
+          <h3>Lorem ipsum dolor sit amet.</h3>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+        </div>
+        <div>
+          <h3>Lorem ipsum dolor sit amet.</h3>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+        </div>
+        <div>
+          <h3>Lorem ipsum dolor sit amet.</h3>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+          <p>Lorem ipsum dolor.</p>
+        </div>
+      </div>
+      <div className="footer_Copyright_div">
+        <p>Copyright © 2025 </p>
+      </div>
     </div>
   );
 };
