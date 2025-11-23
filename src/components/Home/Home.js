7590408721bc2320f55e783659a1e949622b8241
sazzad tomal <sazzadtomal1@gmail.com>
2025-11-23 06:11:12 +0600

    second section done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.css";
 import HeroSection from "../HeroSection/HeroSection";
 import FirstSection from "../FirstSection/FirstSection";
+import SecondSection from "../SecondSection/SecondSection";
 
 const Home = () => {
   return (
     <div className="home_parent_div">
       <HeroSection></HeroSection>
       <FirstSection></FirstSection>
+      <SecondSection></SecondSection>
     </div>
   );
 };
---
+++ src/components/SecondSection/SecondSection.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./SecondSection.css";
+
+const SecondSection = () => {
+  return (
+    <div className="second_section_parent_div">
+      <div className="second_section_text_grid_div">
+        <div>
+          <h4>My Experience</h4>
+          <h1>
+            Experience and <span className="imagination">skill</span>
+          </h1>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
+            ratione reprehenderit, error qui enim sit ex provident
+          </p>
+        </div>
+      </div>
+
+      <div className="second_section_cart_grid_div">
+        <div className="Second_section_cart_div">
+          <h6>2011 - 2014 (Ui designer)</h6>
+          <h3>Google Corporation.</h3>
+          <p>
+            All you need to do your best work together in one package works
+            seamlessly computer
+          </p>
+        </div>
+        <div className="Second_section_cart_div">
+          <h6>2011 - 2014 (Ui designer)</h6>
+          <h3>Google Corporation.</h3>
+          <p>
+            All you need to do your best work together in one package works
+            seamlessly computer
+          </p>
+        </div>
+        <div className="Second_section_cart_div">
+          <h6>2011 - 2014 (Ui designer)</h6>
+          <h3>Google Corporation.</h3>
+          <p>
+            All you need to do your best work together in one package works
+            seamlessly computer
+          </p>
+        </div>
+        <div className="Second_section_cart_div">
+          <h6>2011 - 2014 (Ui designer)</h6>
+          <h3>Google Corporation.</h3>
+          <p>
+            All you need to do your best work together in one package works
+            seamlessly computer
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecondSection;
